d7cb5acc5b81631aefd59c79af51afca3d8cabb7
tuoiii778 <magepewpewpun0suy@hotmail.com>
2024-05-07 14:43:52 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-function uniquePathsWithObstacles(obstacleGrid) {
-  const m = obstacleGrid.length;
-  const n = obstacleGrid[0].length;
-  const dp = Array.from(Array(m), () => Array(n).fill(0));
-  if (obstacleGrid[0][0] === 0) dp[0][0] = 1;
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (obstacleGrid[i][j] === 1) continue;
-      if (i > 0) dp[i][j] += dp[i - 1][j];
-      if (j > 0) dp[i][j] += dp[i][j - 1];
-    }
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
   }
-  return dp[m - 1][n - 1];
+  return false;
 }
