fc18395e4ffb155ce628c1d2231670853e634a93
tuoiii778 <magepewpewpun0suy@hotmail.com>
2024-05-15 04:14:21 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-function combinationSum(candidates, target) {
-  const result = [];
-  backtrack([], 0, 0);
-  return result;
-  function backtrack(combination, start, sum) {
-    if (sum === target) {
-      result.push([...combination]);
-      return;
-    }
-    if (sum > target) return;
-    for (let i = start; i < candidates.length; i++) {
-      combination.push(candidates[i]);
-      backtrack(combination, i, sum + candidates[i]);
-      combination.pop();
-    }
-  }
-}
+const initial = (arr) => arr.slice(0, -1);
