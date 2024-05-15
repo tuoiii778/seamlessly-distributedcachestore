43e3bf49a3460d8976da396751badf7ed19aeeb7
tuoiii778 <magepewpewpun0suy@hotmail.com>
2024-05-15 12:12:33 +0700

    init

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const initial = (arr) => arr.slice(0, -1);
+function sortedListToBST(head) {
+  if (!head) return null;
+  if (!head.next) return new TreeNode(head.val);
+  let slow = head;
+  let fast = head;
+  let prev = null;
+  while (fast && fast.next) {
+    prev = slow;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  const root = new TreeNode(slow.val);
+  prev.next = null;
+  root.left = sortedListToBST(head);
+  root.right = sortedListToBST(slow.next);
+  return root;
+}
